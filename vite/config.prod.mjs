12d4e7b446e2cffaaaaf2e050aa7e56be3225f49
msugawa <msugawa@jops.co.jp>
2025-01-08 20:35:29 +0900

    chore: ビルド設定にGitHub Pages の設定を追加

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -2,11 +2,13 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 
 installGlobals();
 
 // This is the subfolder where the app will be deployed, if you want to change it, you can do it here like this: const subfolder = '/my-subfolder-name/';
-const subfolder = '/';
+const subfolder = '/shire/';
 
 export default defineConfig({
     base: subfolder,
@@ -15,11 +17,18 @@ export default defineConfig({
             ssr: false,
             basename: subfolder,
             
-            buildEnd: () => {
+            buildEnd: (args) => {
                 const line = "---------------------------------------------------------";
                 const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
                 process.stdout.write(`${line}\n${msg}\n${line}\n`);
                 process.stdout.write(`✨ Done ✨\n`);
+
+                if (!args.viteConfig.isProduction) return;
+                const buildPath = args.viteConfig.build.outDir;
+                copyFileSync(
+                  join(buildPath, "index.html"),
+                  join(buildPath, "404.html")
+                );
             }
         }),
         tsconfigPaths(),
