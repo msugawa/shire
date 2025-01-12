f1befd17a0ea2f351193803df8d74d7392bc8c63
msugawa <msugawa@jops.co.jp>
2025-01-12 22:51:37 +0900

    Player paddle and controls

--- app/game/scenes/FirstGame.ts
+++ app/game/scenes/FirstGame.ts
@@ -4,6 +4,7 @@ import { EventBus } from "../EventBus";
 export class FirstGame extends Scene {
   camera: Phaser.Cameras.Scene2D.Camera;
   ball: Phaser.Physics.Arcade.Sprite;
+  paddle: Phaser.Physics.Arcade.Sprite;
 
   constructor() {
     super("FirstGame");
@@ -11,6 +12,7 @@ export class FirstGame extends Scene {
 
   preload(): void {
     this.load.image("ball", "assets/ball.png");
+    this.load.image("paddle", "assets/paddle.png");
   }
 
   create(): void {
@@ -18,15 +20,32 @@ export class FirstGame extends Scene {
     this.camera.setBackgroundColor("#eee");
 
     this.ball = this.physics.add
-      .sprite(50, 50, "ball")
-      .setVelocity(150, 150)
+      .sprite(
+        this.game.canvas.width * 0.5,
+        this.game.canvas.height - 100,
+        "ball",
+      )
+      .setOrigin(0.5)
+      .setVelocity(150, -150)
       .setCollideWorldBounds(true)
       .setBounce(1);
+
+    this.paddle = this.physics.add
+      .sprite(
+        this.game.canvas.width * 0.5,
+        this.game.canvas.height - 50,
+        "paddle",
+      )
+      .setOrigin(0.5, 1)
+      .setImmovable(true);
 
     EventBus.emit("current-scene-ready", this);
   }
 
-  update() {}
+  update() {
+    this.physics.collide(this.ball, this.paddle);
+    this.paddle.x = this.input.x || this.game.canvas.width * 0.5;
+  }
 
   changeScene() {
     this.scene.start("MainMenu");
